f54f5cb69e9a73e93af3e1eabfd23d9e74658379
Boootyshaker9000 <relich@post.cz>
2026-01-06 18:13:36 +0100

    Removed unused import.

--- src/dao/LoanDAO.java
+++ src/dao/LoanDAO.java
@@ -3,7 +3,6 @@ package dao;
 import conn.DatabaseConnector;
 import models.*;
 import java.sql.*;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
